33f1ee1cde7b4b26bb13bceb26115b07b5d53186
Admin-PC\Admin <itmo1652@mail.ru>
2022-05-17 01:31:59 +0300

    commit 0019

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import music.player.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int start = 1;
+        peaceOfSound(start);
+        peaceOfSound(start - 2);
+        //Player player = new Player();
+        //player.playSound(0, 500, 80, 69, 72, 76);
+        //player.playSound(0, 500, 80, 69, 74, 77);
+        //player.playSound(0, 500, 80, 67, 71, 74);
+        //player.playSound(0, 500, 80, 67, 72, 76);
+        //player.close();
     }
+    public static void peaceOfSound(int x) {
+        Player player = new Player();
+        for(int i = 0; i < 16; i++) {
+            player.playSound(0, 50, 80, x + 69);
+            player.playSound(0, 50, 80, x + 72);
+            player.playSound(0, 50, 80, x + 76);
+            player.playSound(0, 50, 80, x + 81);
+
+
+        }
+        player.close();
+    }
+
 }
